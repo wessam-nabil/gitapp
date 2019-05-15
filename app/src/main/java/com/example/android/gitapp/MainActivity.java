5330db588a03e5ed7989ef1f6e05c93b570c3a66
wessam-nabil <wessamnabill8@gmail.com>
2019-05-15 09:07:21 +0300

    second commit

--- app/src/main/java/com/example/android/gitapp/MainActivity.java
+++ app/src/main/java/com/example/android/gitapp/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("mytag", "first");
-        Log.i("mytag", "second");
+        Log.i("mytag", "secondd");
     }
 }
